343d3217f5e1e9034c3fc32d4e1fde9fb4be42a4
함소연 <soyeun3377@naver.com>
2023-11-12 13:33:25 +0900

    :heavy_plus_sign: Add Exception Handler

--- senabo-spring/src/main/java/com/senabo/exception/handler/GlobalExceptionHandler.java
+++ senabo-spring/src/main/java/com/senabo/exception/handler/GlobalExceptionHandler.java
@@ -2,23 +2,24 @@ package com.senabo.exception.handler;
 
 import com.senabo.exception.dto.ExceptionDto;
 import com.senabo.exception.message.ExceptionMessage;
-import com.senabo.exception.model.TokenCheckFailException;
+import com.senabo.exception.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
 //    @ExceptionHandler(value = Exception.class)
 //    public ResponseEntity<ExceptionDto> exceptionHandler(Exception exception){
 //        return ExceptionDto.
 //    }
-//    @ExceptionHandler(TokenCheckFailException.class)
-//    protected ResponseEntity<ExceptionDto> handleTokenCheckFailException(ExceptionMessage exceptionMessage){
-//        return ExceptionDto.toResponseEntity(exceptionMessage);
-//    }
+    @ExceptionHandler({DataException.class, TokenCheckFailException.class, TokenNotFoundException.class, UserAuthException.class, UserException.class})
+    protected ResponseEntity<ExceptionDto> handle(ExceptionMessage exceptionMessage){
+        return ExceptionDto.toResponseEntity(exceptionMessage);
+    }
 
 
 }
